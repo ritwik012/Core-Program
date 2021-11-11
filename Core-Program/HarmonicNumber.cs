e9470ddb9e0cbc8bb042774fd5e7b825d3fa1a6d
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-11-12 00:08:53 +0530

    [Rutwik]Added:HarmonicNumber CoreProgram Features Implemented

---
+++ Core-Program/HarmonicNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorePrograms
+{
+    class HarmonicNumber
+    {
+        public void Find()
+        {
+            double harmonicNumber = 0;
+            Console.WriteLine("Enter the Harmonic Number as N Where N Should not be 0");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < num; i++)
+            {
+                harmonicNumber += 1 / (float)i;
+            }
+            Console.WriteLine(Math.Round(harmonicNumber, 2)); //Rounds a double-precision floating-point value to a specified number of fractional
+                                                              //digits, and uses the specified rounding convention for midpoint values 
+        }
+    }
+}
