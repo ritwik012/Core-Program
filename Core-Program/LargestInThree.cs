497a4cbc7e413b4d868583505ab92b98fd19742e
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-11-12 00:35:53 +0530

    [Rutwik]Added:LaregestAmongThree CoreProgram Features Implemented

---
+++ Core-Program/LargestInThree.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorePrograms
+{
+    class LargestInThree
+    {
+        public void Check()
+        {
+            Console.Write("Enter the 1st number :");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the 2nd number :");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the 3rd Number :");
+            int numThree = Convert.ToInt32(Console.ReadLine());
+
+
+            if (numOne > numTwo && numOne > numThree)
+                Console.WriteLine($"number {numOne} is maximum");
+            else if (numTwo > numOne && numTwo > numThree)
+                Console.WriteLine($"number {numTwo} is maximum");
+            else
+                Console.WriteLine($"number  {numThree} is maximum");
+        }
+    }
+}
