309b6fe66b10ce20a1b9859e7a05230d67f8566e
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-11-12 00:16:40 +0530

    [Rutwik]Added:QuotientRemainder CoreProgram Features Implemented

---
+++ Core-Program/QuotientRemainder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorePrograms
+{
+    class QuotientRemainder
+    {
+        public void Calculation()
+        {
+            int qutoient, remainder;
+            Console.WriteLine("Enter The Divindend ");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter The Divisor ");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+            qutoient = dividend / divisor;
+            remainder = dividend % divisor;
+            Console.WriteLine("Here is the Quotient is:" + qutoient);
+            Console.WriteLine("Here is the Remainder is: " + remainder);
+        }
+    }
+}
