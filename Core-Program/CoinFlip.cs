14d0d1a5a97f4048da9e2e8401eef867b664371f
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-11-11 23:56:01 +0530

    [Rutwik]Added:CoinFlip CoreProgram Features Implemented

---
+++ Core-Program/CoinFlip.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorePrograms
+{
+    class CoinFlip
+    {
+        public void Play()
+        {
+            Console.WriteLine("Enter the Number of times want to flip the coin");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Random rand = new Random();
+            int headCount = 0, tailCount = 0;
+            double percentage;
+            for (int i = 0; i < num; i++)
+            {
+                double flip = rand.NextDouble();
+                if (flip < 0.5)
+                {
+                    Console.WriteLine("Heads");
+                    headCount++;
+                    percentage = (headCount / num) * 100;
+                    Console.WriteLine("Percentage of Heads vs Tails is: " + percentage);
+                }
+                else
+                {
+                    Console.WriteLine("Tails");
+                    tailCount++;
+                    percentage = (tailCount / num) * 100;
+                    Console.WriteLine("Percentage of Heads vs Tails is: " + percentage);
+                }
+
+            }
+        }
+    }
+}
---
+++ Core-Program/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CorePrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter your Choice Number to Execute the Program Press- 1-CoinFlip, 2-LeapYear, 3-PowerOfTwo, 4-HarmonicNumber, 5-Factors, 6-QuotientRemainder, 7-SwapNumber, 8-EvenOdd, 9-VowelConsonent,10-LargestOne,11-Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        CoinFlip cf = new CoinFlip();
+                        cf.Play();
+                        break;
+                    case 2:
+                        LeapYear lp = new LeapYear();
+                        lp.Check();
+                        break;
+                    case 3:
+                        PowerOfTwo pft = new PowerOfTwo();
+                        pft.Calculation();
+                        break;
+                    case 4:
+                        HarmonicNumber hn = new HarmonicNumber();
+                        hn.Find();
+                        break;
+                    case 5:
+                        Factors fc = new Factors();
+                        fc.Calculation();
+                        break;
+                    case 6:
+                        QuotientRemainder qr = new QuotientRemainder();
+                        qr.Calculation();
+                        break;
+                    case 7:
+                        SwapNumber sn = new SwapNumber();
+                        sn.Calculation();
+                        break;
+                    case 8:
+                        EvenOdd ev = new EvenOdd();
+                        ev.Calculation();
+                        break;
+                    case 9:
+                        VowelConsonent vc = new VowelConsonent();
+                        vc.Check();
+                        break;
+                    case 10:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
