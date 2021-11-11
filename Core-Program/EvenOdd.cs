dcf9c036c64ff033f094ce15ad80ca4ee9e36a47
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-11-12 00:23:06 +0530

    [Rutwik]Added:EvenOdd CoreProgram Features Implemented

---
+++ Core-Program/EvenOdd.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorePrograms
+{
+    class EvenOdd
+    {
+        public void Calculation()
+        {
+            Console.WriteLine("Enter the Number to Check it is Even or Odd");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine(num + "is a even no.");
+            }
+            else
+            {
+                Console.WriteLine(num + "is a odd no.");
+            }
+        }
+    }
+}
